948cc076f0842c2b91fe703653257ce5f0aad4ba
Steve King <sking@Steves-MacBook-Pro.local>
2021-07-11 15:04:23 -0400

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,43 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+candidate_options = []
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here.
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    # Print each row in the CSV file.
+    
+        # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+# Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+         
+
+        # 3. Print the total votes.
+print(total_votes)
+
+# Print the candidate list.
+print(candidate_options)
---
+++ Python_practice.py
@@ -0,0 +1,12 @@
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w")
+
+# Using the with statement open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
